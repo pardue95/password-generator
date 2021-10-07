be21eec303491e29e7b117cabcea294f61ca07ba
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-10-07 13:58:32 -0500

    working on password length

--- assests/js/script.js
+++ assests/js/script.js
@@ -4,73 +4,103 @@ var lowerCase = " ";
 var upperCase = " ";
 var numeric = " ";
 var symbolRandom = " ";
+var generateBtn = document.querySelector("#generate");
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
+// const randomFunc = {
+//   lower: getRandomLower,
+//   upper: getRandomUpper,
+//   number: getRandomNumber,
+//   symbol: getRandomSymbol
+// };
 
 // Generates a random lowercase character
 function getRandomLower() {
-var getRandomLower = String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-lowerCase = getRandomLower;
-return lowerCase;
+	var getRandomLower = String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+	lowerCase = getRandomLower;
+	return lowerCase;
 };
 
-// Generates a random lowercase character
+// Generates a random uppercase character
 function getRandomUpper() {
-  var getRandomUpper =String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-  upperCase = getRandomUpper;
-  return upperCase;
+	var getRandomUpper = String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+	upperCase = getRandomUpper;
+	return upperCase;
 };
 
-// Generates a random lowercase character
+// Generates a random number 0-9
 function getRandomNumber() {
-  var getRandomNumber = String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-  numeric = getRandomNumber;
- return numeric; 
+	var getRandomNumber = String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+	numeric = getRandomNumber;
+	return numeric;
 };
 
-// Generates a random lowercase character
+// Generates a random symbol
 function getRandomSymbol() {
-  var length = 1
-  var charset = "!@#$%^&*(){}[]=<>/,.";
-  symbolRandom = "";
-  
-  for (var i = 0, n = charset.length; i < length; ++i) {
-    symbolRandom += charset.charAt(Math.floor(Math.random() * n));
-}
-return symbolRandom;
-} ;
+	var length = 1
+	var charset = "!@#$%^&*(){}[]=<>/,.";
+	symbolRandom = "";
+  var getRandomSymbol = confirm("Would you like to add symbols?");
+  if (getRandomSymbol === false) { 
+    return;} else 
+    {for (var i = 0, n = charset.length; i < length; ++i) {
+      symbolRandom += charset.charAt(Math.floor(Math.random() * n))
+    } return symbolRandom} 
+
+};
+
+
+// git checko
+
+
 //  MAIN FUNCTION
 function passGen() {
-  var promptPassGen = confirm("Would you like to generate a new password");
-  console.log(promptPassGen);
-  if (promptPassGen === true) {
-    var selectLength = prompt("How long do you want your password to be? Please choose a number between 8 and 128.");
-//    console.log(selectLength);
+	var promptPassGen = confirm("Would you like to generate a new password");
+	//console.log(promptPassGen);
+	if (promptPassGen === true) {
+		var selectLength = prompt("How long do you want your password to be? Please choose a number between 8 and 128.");
+//	console.log(selectLength);
+		if (selectLength < 8 || selectLength > 128) {
+			alert("You must choose a number between 8 and 128!");
+			return false;
+		} else {
     passLength = selectLength;
-    return passLength;
- };
+      return };
+	};
 };
 
-//passGen();
-//getRandomLower();
-//getRandomUpper();
-//getRandomNumber();
+function generatePassword() {
+  passGen();
+  console.log(passLength);
+  getRandomSymbol()
+  console.log(symbolRandom);  
+  getRandomUpper();
+getRandomNumber()
 getRandomSymbol();
-//console.log(passLength);
-console.log(lowerCase);
-console.log(upperCase);
-console.log(numeric);
-console.log(symbolRandom);
-console.log(randomFunc[0]);
+// console.log(passLength);
+// console.log(lowerCase);
+// console.log(upperCase);
+// console.log(numeric);
+// console.log(symbolRandom);
+// console.log(passLength);
+};
 
 
+generateBtn.addEventListener("click", function () {
+	generatePassword();
+});
 
+// // //passGen();
+//  getRandomLower();
+// getRandomUpper();
+// getRandomNumber()
+// getRandomSymbol();
+// console.log(passLength);
+// console.log(lowerCase);
+// console.log(upperCase);
+// console.log(numeric);
+// console.log(symbolRandom);
 
+// console.log(passLength);
 
 
 /*
